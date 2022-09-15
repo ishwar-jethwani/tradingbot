c117b9dda88a15409b9ab5ae2c2951443a6721b8
ishwar <ishwar@zoconut.com>
2022-09-15 08:19:58 +0530

    chnage in loginview

--- account/views.py
+++ account/views.py
@@ -13,21 +13,8 @@ class CustomLoginView(LoginView):
         orginal_response = super().get_response()
         mydata = self.get_user()
         orginal_response.data.update(mydata)
-        email = mydata["email"]
-        username = mydata["username"]
-        # subject = "Ultra Creation Sending Email"
-        # message = "Hi %s! Welcome to UltraXpert" % email
-        # htmly = get_template("welcome-email.html")
-        # htmly = htmly.render({"username":username})
-        User.objects.filter(email=email).update(is_verified=True)
-        # send_mail(
-        #     from_email = None,
-        #     recipient_list = [email],
-        #     subject =subject,
-        #     html_message = htmly,
-        #     message = message
-        #     )
-
         return orginal_response
+
+
 
 
